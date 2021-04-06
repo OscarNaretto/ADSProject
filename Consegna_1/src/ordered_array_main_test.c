a523aa88dd061b249d846645061e5edb6ff60acd
Vincenzo Milordo <vincenzo.milordo@edu.unito.it>
2021-04-06 16:14:43 +0200

    Test

--- Consegna_1/src/ordered_array_main_test.c
+++ Consegna_1/src/ordered_array_main_test.c
@@ -1,5 +1,7 @@
+#include <stdlib.h>
 #include "unity.h"
 #include "ordered_array.h"
+
 
 static int precedes_int(void *i1_p, void *i2_p) {
   int *int1_p = (int*)i1_p;
@@ -50,7 +52,7 @@ static void test_ordered_array_add_get_one_el() {
   ordered_array_free_memory(ordered_array_int);
 }
 
-
+/*
 static void test_ordered_array_add_get_three_el() {
   int i1 = -12;
   int i2 = 0;
@@ -66,10 +68,10 @@ static void test_ordered_array_add_get_three_el() {
   for (unsigned long i=0; i < 3; ++i) {
     act_arr[i] = (int*)ordered_array_get(ordered_array_int, i);
   }
-  TEST_ASSERT_EQUAL_PTR_ARRAY(exp_arr, act_arr, 3); //(I,j,z) sono i parametri di entrabi gli array , z sono gli elemnti da controllare 
+  TEST_ASSERT_EQUAL_PTR_ARRAY(exp_arr, act_arr, 3);//(I,j,z) sono i parametri di entrabi gli array , z sono gli elemnti da controllare
   free(act_arr);
   ordered_array_free_memory(ordered_array_int);
-}
+}*/
 
 
 static void test_algorithm(){
@@ -95,6 +97,29 @@ static void test_algorithm(){
 }
 
 
+static void test_algorithm_reversed(){
+    int i1 = 2;
+    int i2 = 0;
+    int i3 = 4;
+    int i4 = 23;
+    int i5 = 10;
+
+    OrderedArray *ordered_array_int = ordered_array_create(precedes_int);
+    ordered_array_add(ordered_array_int, &i3);
+    ordered_array_add(ordered_array_int, &i5);
+    ordered_array_add(ordered_array_int, &i1);
+    ordered_array_add(ordered_array_int, &i2);
+    ordered_array_add(ordered_array_int, &i4);
+    sortingAlgorithm(ordered_array_int,precedes_int, 0, ordered_array_size(ordered_array_int) - 1);
+    TEST_ASSERT_EQUAL_PTR(&i2,ordered_array_get(ordered_array_int, 0));
+    TEST_ASSERT_EQUAL_PTR(&i1,ordered_array_get(ordered_array_int, 1));
+    TEST_ASSERT_EQUAL_PTR(&i3,ordered_array_get(ordered_array_int, 2));
+    TEST_ASSERT_EQUAL_PTR(&i5,ordered_array_get(ordered_array_int, 3)); 
+    TEST_ASSERT_EQUAL_PTR(&i4,ordered_array_get(ordered_array_int, 4));
+    ordered_array_free_memory(ordered_array_int);
+}
+
+
 
 
 int main(){
@@ -105,8 +130,9 @@ int main(){
     RUN_TEST(test_ordered_array_size_one_el);
     RUN_TEST(test_ordered_array_size_two_el);
     RUN_TEST(test_ordered_array_add_get_one_el);
-    RUN_TEST(test_ordered_array_add_get_three_el);
+    //RUN_TEST(test_ordered_array_add_get_three_el);
     RUN_TEST(test_algorithm);
+    RUN_TEST(test_algorithm_reversed);
     return UNITY_END();
 }
 
