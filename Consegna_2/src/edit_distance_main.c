a049e62ca78cc3c7d207b3f1591f77042339a94a
“Oscar <oscar.naretto@edu.unit.it>
2021-04-27 10:14:36 +0200

    updated non-dynamic method

--- Consegna_2/src/edit_distance_main.c
+++ Consegna_2/src/edit_distance_main.c
@@ -5,7 +5,7 @@
 
 int min(int a, int b, int c) {
 	int min = a;
-    
+
 	if(b < min)
 	    min = b;
 	if(c < min)
@@ -14,7 +14,7 @@ int min(int a, int b, int c) {
 }
 
 int edit_distance(char *string1, char *string2){
-    int edit_distance_noop;
+    int no_op;
     if (strlen(string1) == 0){
         return strlen(string2);
     }
@@ -23,28 +23,26 @@ int edit_distance(char *string1, char *string2){
         return strlen(string1);
     }
 
-    char rest1[strlen(string1) - 1];        //oppure incremento puntatore della string
+    /*char rest1[strlen(string1) - 1];        //oppure incremento puntatore della string
     char rest2[strlen(string2) - 1];        //valutare cambio approccio
     strcpy(rest1, string1 + 1);
-    strcpy(rest2, string2 + 1);
-
+    strcpy(rest2, string2 + 1);*/
 
     if (string1[0] == string2[0]){
-        edit_distance_noop = edit_distance(rest1, rest2);
+        no_op = edit_distance(string1+1, string2+1);
     } else {
-        edit_distance_noop = INT_MAX;
+        no_op = INT_MAX;
     }
-    
-    return min(1 + edit_distance(string1, rest2), 1 + edit_distance(rest1, string2), edit_distance(rest1, rest2));
+    return min(1 + edit_distance(string1, string2+1), 1 + edit_distance(string1+1, string2), no_op);
 }
 
 int main(int argc, char const *argv[]){
 
     printf("Parole scelte: tassa  passato \n");
-    char string1[] = "pioppo";
-    char string2[] = "pioppo";
+    char string1[] = "passato";
+    char string2[] = "passato";
     
-    int res = edit_distance(string1, string2);
+    int res = edit_distance(string1, string2);      //primi tentativi
 
     printf("Risultato: %d\n", res);
 
