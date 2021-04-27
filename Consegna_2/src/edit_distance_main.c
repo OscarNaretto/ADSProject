e584018acc2c05c96b8113b2b5bdd6c25e084677
“Oscar <oscar.naretto@edu.unit.it>
2021-04-27 11:41:05 +0200

    versione definitiva non dinamica

--- Consegna_2/src/edit_distance_main.c
+++ Consegna_2/src/edit_distance_main.c
@@ -12,20 +12,11 @@ int min(int a, int b, int c) {
     }
 }
 
+//parte dall'inizio della stringa, per comodità di lettura
 int edit_distance(char *string1, char *string2){
     int no_op;
-    if (strlen(string1) == 0){
-        return strlen(string2);
-    }
-
-    if (strlen(string2) == 0){
-        return strlen(string1);
-    }
-
-    /*char rest1[strlen(string1) - 1];        //oppure incremento puntatore della string
-    char rest2[strlen(string2) - 1];        //valutare cambio approccio
-    strcpy(rest1, string1 + 1);
-    strcpy(rest2, string2 + 1);*/
+    if (strlen(string1) == 0) return strlen(string2);
+    if (strlen(string2) == 0) return strlen(string1);
 
     if (string1[0] == string2[0]){
         no_op = edit_distance(string1+1, string2+1);
@@ -39,10 +30,9 @@ int main(int argc, char const *argv[]){
 
     printf("Parole scelte: tassa  passato \n");
     char string1[] = "tassa";
-    char string2[] = "passato";
-    
-    int res = edit_distance(string1, string2);      //primi tentativi
+    char string2[] = "passato"; 
 
+    int res = edit_distance(string1, string2);
     printf("Risultato: %d\n", res);
 
     return EXIT_SUCCESS;
