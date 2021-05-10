ca2358fd6d4d05e64ce26c2d15112b91a5d2af39
Vincenzo Milordo <vincenzo.milordo@edu.unito.it>
2021-05-10 22:56:29 +0200

    test

--- Consegna_2/src/edit_distance_test.c
+++ Consegna_2/src/edit_distance_test.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "unity.h"
 #include "edit_distance_lib.h"
+#define MWL 10
+
 
 int **recursive_calls_table;
 
@@ -49,61 +51,192 @@ static void test_edit_distance_example_name_v(){
 /*--------TEST PARTE DINAMICA --------------------------------------------------*/
 
 static void test_edit_distance_dyn_empty(){
-
-    recursive_calls_table = (int **)malloc(MAX_WORD_LENGTH * sizeof(int*));  
-    for (int i = 0; i < MAX_WORD_LENGTH; i++){
-        recursive_calls_table[i] = malloc(MAX_WORD_LENGTH * sizeof(int));
-            for (int j = 0; j < MAX_WORD_LENGTH; j++){
+    recursive_calls_table = (int **)malloc(MWL* sizeof(int*));  
+    for (int i = 0; i < MWL; i++){
+        recursive_calls_table[i] = malloc(MWL * sizeof(int));
+            for (int j = 0; j < MWL; j++){
                 recursive_calls_table[i][j]= -1;
             }
     }
 
     TEST_ASSERT_EQUAL_INT(0, edit_distance_dynamic("","",0,0,recursive_calls_table));
 
-     for (int i = 0; i < MAX_WORD_LENGTH; i++){
+     for (int i = 0; i < MWL; i++){
         free(recursive_calls_table[i]);
     }
     free(recursive_calls_table);
 }
 
 static void test_edit_distance_dyn_1(){
-    TEST_ASSERT_EQUAL_INT(1,edit_distance("i",""));
+    recursive_calls_table = (int **)malloc(MWL* sizeof(int*));  
+    for (int i = 0; i < MWL; i++){
+        recursive_calls_table[i] = malloc(MWL * sizeof(int));
+            for (int j = 0; j < MWL; j++){
+                recursive_calls_table[i][j]= -1;
+            }
+    }
+
+    TEST_ASSERT_EQUAL_INT(1,edit_distance_dynamic("i","",1,0,recursive_calls_table));
+
+    for (int i = 0; i < MWL; i++){
+        free(recursive_calls_table[i]);
+    }
+    free(recursive_calls_table);
 }
 
 static void test_edit_distance_dny_2(){
-    TEST_ASSERT_EQUAL_INT(2,edit_distance("","ii"));
+    recursive_calls_table = (int **)malloc(MWL* sizeof(int*));  
+    for (int i = 0; i < MWL; i++){
+        recursive_calls_table[i] = malloc(MWL * sizeof(int));
+            for (int j = 0; j < MWL; j++){
+                recursive_calls_table[i][j]= -1;
+            }
+    }
+
+    TEST_ASSERT_EQUAL_INT(2,edit_distance_dynamic("","ii",0,2,recursive_calls_table));
+
+    for (int i = 0; i < MWL; i++){
+        free(recursive_calls_table[i]);
+    }
+    free(recursive_calls_table);
 }
 
 static void test_edit_distance_dny_3(){
-    TEST_ASSERT_EQUAL_INT(3,edit_distance("iii",""));
+    recursive_calls_table = (int **)malloc(MWL* sizeof(int*));  
+    for (int i = 0; i < MWL; i++){
+        recursive_calls_table[i] = malloc(MWL * sizeof(int));
+            for (int j = 0; j < MWL; j++){
+                recursive_calls_table[i][j]= -1;
+            }
+    }
+
+    TEST_ASSERT_EQUAL_INT(3,edit_distance_dynamic("iii","",3,0,recursive_calls_table));
+
+    for (int i = 0; i < MWL; i++){
+        free(recursive_calls_table[i]);
+    }
+    free(recursive_calls_table);
 }
+
 static void test_edit_distance_dny_example_1(){
-    TEST_ASSERT_EQUAL_INT(1,edit_distance("casa","cassa"));
+    recursive_calls_table = (int **)malloc(MWL* sizeof(int*));  
+    for (int i = 0; i < MWL; i++){
+        recursive_calls_table[i] = malloc(MWL * sizeof(int));
+            for (int j = 0; j < MWL; j++){
+                recursive_calls_table[i][j]= -1;
+            }
+    }
+
+    TEST_ASSERT_EQUAL_INT(1,edit_distance_dynamic("casa","cassa",5,6,recursive_calls_table));
+
+    for (int i = 0; i < MWL; i++){
+        free(recursive_calls_table[i]);
+    }
+    free(recursive_calls_table);
 }
 
 static void test_edit_distance_dny_example_2(){
-    TEST_ASSERT_EQUAL_INT(2,edit_distance("casa","cara"));
+    recursive_calls_table = (int **)malloc(MWL* sizeof(int*));  
+    for (int i = 0; i < MWL; i++){
+        recursive_calls_table[i] = malloc(MWL * sizeof(int));
+            for (int j = 0; j < MWL; j++){
+                recursive_calls_table[i][j]= -1;
+            }
+    }
+
+    TEST_ASSERT_EQUAL_INT(2,edit_distance_dynamic("casa","cara",5,5,recursive_calls_table));
+
+    for (int i = 0; i < MWL; i++){
+        free(recursive_calls_table[i]);
+    }
+    free(recursive_calls_table);
 }
 
 static void test_edit_distance_dny_example_3(){
-    TEST_ASSERT_EQUAL_INT(4,edit_distance("tassa","passato"));
+    recursive_calls_table = (int **)malloc(MWL* sizeof(int*));  
+    for (int i = 0; i < MWL; i++){
+        recursive_calls_table[i] = malloc(MWL * sizeof(int));
+            for (int j = 0; j < MWL; j++){
+                recursive_calls_table[i][j]= -1;
+            }
+    }
+
+    TEST_ASSERT_EQUAL_INT(4,edit_distance_dynamic("tassa","passato",6,8,recursive_calls_table));
+
+    for (int i = 0; i < MWL; i++){
+        free(recursive_calls_table[i]);
+    }
+    free(recursive_calls_table);
 }
 
 static void test_edit_distance_dny_example_4(){
-    TEST_ASSERT_EQUAL_INT(0,edit_distance("pioppo","pioppo"));
+    recursive_calls_table = (int **)malloc(MWL* sizeof(int*));  
+    for (int i = 0; i < MWL; i++){
+        recursive_calls_table[i] = malloc(MWL * sizeof(int));
+            for (int j = 0; j < MWL; j++){
+                recursive_calls_table[i][j]= -1;
+            }
+    }
+
+    TEST_ASSERT_EQUAL_INT(0,edit_distance_dynamic("pioppo","pioppo",7,7, recursive_calls_table));
+
+    for (int i = 0; i < MWL; i++){
+        free(recursive_calls_table[i]);
+    }
+    free(recursive_calls_table);
 }
 
 static void test_edit_distance_dny_example_name_d(){
-    TEST_ASSERT_EQUAL_INT(3,edit_distance("dani","danno"));
+    recursive_calls_table = (int **)malloc(MWL* sizeof(int*));  
+    for (int i = 0; i < MWL; i++){
+        recursive_calls_table[i] = malloc(MWL * sizeof(int));
+            for (int j = 0; j < MWL; j++){
+                recursive_calls_table[i][j]= -1;
+            }
+    }
+
+    TEST_ASSERT_EQUAL_INT(3,edit_distance_dynamic("dani","danno",5,6,recursive_calls_table));
+
+    for (int i = 0; i < MWL; i++){
+        free(recursive_calls_table[i]);
+    }
+    free(recursive_calls_table);
 }
 
 static void test_edit_distance_dny_example_name_o(){
-    TEST_ASSERT_EQUAL_INT(5,edit_distance("oscar","omahr")); 
+    recursive_calls_table = (int **)malloc(MWL* sizeof(int*));  
+    for (int i = 0; i < MWL; i++){
+        recursive_calls_table[i] = malloc(MWL * sizeof(int));
+            for (int j = 0; j < MWL; j++){
+                recursive_calls_table[i][j]= -1;
+            }
+    }
+
+    TEST_ASSERT_EQUAL_INT(4,edit_distance_dynamic("oscar","omahr",6,6,recursive_calls_table)); 
+
+    for (int i = 0; i < MWL; i++){
+        free(recursive_calls_table[i]);
+    }
+    free(recursive_calls_table);
 }
 
 static void test_edit_distance_dny_example_name_v(){
-    TEST_ASSERT_EQUAL_INT(3,edit_distance("vince","vincenzo"));
+    recursive_calls_table = (int **)malloc(MWL* sizeof(int*));  
+    for (int i = 0; i < MWL; i++){
+        recursive_calls_table[i] = malloc(MWL * sizeof(int));
+            for (int j = 0; j < MWL; j++){
+                recursive_calls_table[i][j]= -1;
+            }
+    }
+
+    TEST_ASSERT_EQUAL_INT(3,edit_distance_dynamic("vince","vincenzo",6,9,recursive_calls_table));
+
+    for (int i = 0; i < MWL; i++){
+        free(recursive_calls_table[i]);
+    }
+    free(recursive_calls_table);
 }
+
 
 
 int main(){
@@ -121,9 +254,9 @@ int main(){
     RUN_TEST(test_edit_distance_example_name_d);
     RUN_TEST(test_edit_distance_example_name_o);
     RUN_TEST(test_edit_distance_example_name_v);
-
+/*---------- TEST PARTE DINAMICA -------------*/
     RUN_TEST(test_edit_distance_dyn_empty);
-   /* RUN_TEST(test_edit_distance_dyn_1);
+    RUN_TEST(test_edit_distance_dyn_1);
     RUN_TEST(test_edit_distance_dny_2);
     RUN_TEST(test_edit_distance_dny_3);
     RUN_TEST(test_edit_distance_dny_example_1);
@@ -133,9 +266,7 @@ int main(){
     RUN_TEST(test_edit_distance_dny_example_4);
     RUN_TEST(test_edit_distance_dny_example_name_d);
     RUN_TEST(test_edit_distance_dny_example_name_o);
-    RUN_TEST(test_edit_distance_dny_example_name_v);*/
-
-
+    RUN_TEST(test_edit_distance_dny_example_name_v);
 
     return UNITY_END();
 }
