6872d78c49d873d1619fa7979e07e9c3d822ad0d
“Oscar <oscar.naretto@edu.unit.it>
2021-05-10 19:00:26 +0200

    updated dictionary test

--- Consegna_2/src/dictionary_test.c
+++ Consegna_2/src/dictionary_test.c
@@ -11,61 +11,62 @@ static void test_dictionary_array_size_zero_el(){
 
 static void test_dictionary_array_size_one_el(){
     Dictionary *dictionary_array = dictionary_create();
-    dictionary_add(dictionary_array, "one");
+    dictionary_add(dictionary_array, "aaa");
     TEST_ASSERT_EQUAL_INT(1, dictionary_array_size(dictionary_array));
     dictionary_array_free(dictionary_array);
 }
 
 static void test_dictionary_array_size_two_el(){
     Dictionary * dictionary_array = dictionary_create();
-    dictionary_add(dictionary_array, "one");
-    dictionary_add(dictionary_array, "two");
+    dictionary_add(dictionary_array, "aaa");
+    dictionary_add(dictionary_array, "bbb");
     TEST_ASSERT_EQUAL_INT(2, dictionary_array_size(dictionary_array));
     dictionary_array_free(dictionary_array);
 }
 
 static void test_present(){ 
     Dictionary * dictionary_array = dictionary_create();
-    dictionary_add(dictionary_array,"one");
-    dictionary_add(dictionary_array,"two");
-    TEST_ASSERT_EQUAL_INT(1,dictionary_is_present(dictionary_array,"one"));
+    dictionary_add(dictionary_array,"aaa");
+    dictionary_add(dictionary_array,"bbb");
+    dictionary_add(dictionary_array,"ccc");
+    TEST_ASSERT_EQUAL_INT(1,dictionary_is_present(dictionary_array,"bbb"));
     dictionary_array_free(dictionary_array);
 }
 
 static void test_not_present(){
     Dictionary * dictionary_array = dictionary_create();
-    dictionary_add(dictionary_array,"one");
-    dictionary_add(dictionary_array,"two");
-    dictionary_add(dictionary_array,"three");
-    TEST_ASSERT_EQUAL_INT(0,dictionary_is_present(dictionary_array,""));
+    dictionary_add(dictionary_array,"aaa");
+    dictionary_add(dictionary_array,"bbb");
+    dictionary_add(dictionary_array,"ccc");
+    TEST_ASSERT_EQUAL_INT(0,dictionary_is_present(dictionary_array,"aas"));
     dictionary_array_free(dictionary_array);
 }
 
 static void test_search(){
     Dictionary * dictionary_array = dictionary_create();
-    dictionary_add(dictionary_array,"one");
-    dictionary_add(dictionary_array,"two");
-    TEST_ASSERT_EQUAL_INT(0,dictionary_search(dictionary_array,0,dictionary_array_size(dictionary_array),"one")); 
+    dictionary_add(dictionary_array,"aaa");
+    dictionary_add(dictionary_array,"bbb");
+    TEST_ASSERT_EQUAL_INT(0,dictionary_search(dictionary_array,0,dictionary_array_size(dictionary_array),"aaa")); 
     dictionary_array_free(dictionary_array);
 }
 
 static void test_search_two(){
     Dictionary * dictionary_array = dictionary_create();
-    dictionary_add(dictionary_array,"one");
-    dictionary_add(dictionary_array,"two");
-    dictionary_add(dictionary_array,"three");
-    dictionary_add(dictionary_array,"for");
-    TEST_ASSERT_EQUAL_INT(2,dictionary_search(dictionary_array,0,dictionary_array_size(dictionary_array),"three")); 
+    dictionary_add(dictionary_array,"aaa");
+    dictionary_add(dictionary_array,"bbb");
+    dictionary_add(dictionary_array,"ccc");
+    dictionary_add(dictionary_array,"ddd");
+    TEST_ASSERT_EQUAL_INT(2,dictionary_search(dictionary_array,0,dictionary_array_size(dictionary_array),"ccc")); 
     dictionary_array_free(dictionary_array);
 }
 
 static void test_get_element(){
     Dictionary * dictionary_array = dictionary_create();
-    dictionary_add(dictionary_array,"one");
-    dictionary_add(dictionary_array,"two");
-    dictionary_add(dictionary_array,"three");
-    dictionary_add(dictionary_array,"for");
-    TEST_ASSERT_EQUAL_STRING("for",dictionary_get_elem(dictionary_array,3)); 
+    dictionary_add(dictionary_array,"aaa");
+    dictionary_add(dictionary_array,"bbb");
+    dictionary_add(dictionary_array,"ccc");
+    dictionary_add(dictionary_array,"ddd");
+    TEST_ASSERT_EQUAL_STRING("ddd",dictionary_get_elem(dictionary_array,3)); 
     dictionary_array_free(dictionary_array);
 
 }
