d2d888e1a0ef1d40f1b71b7273a778ee011cd141
Vincenzo Milordo <vincenzo.milordo@edu.unito.it>
2021-05-10 18:18:22 +0200

    test

--- Consegna_2/src/dictionary_test.c
+++ Consegna_2/src/dictionary_test.c
@@ -30,17 +30,47 @@ static void test_present(){ // da vedere
     dictionary_add(dictionary_array,"one");
     dictionary_add(dictionary_array,"two");
     TEST_ASSERT_EQUAL_INT(1,dictionary_is_present(dictionary_array,"one")); // 1 perchè elemento è presente
+    dictionary_array_free(dictionary_array);
 }
-
+/*
 static void test_not_present(){ // da vedere
     Dictionary * dictionary_array = dictionary_create();
     dictionary_add(dictionary_array,"one");
     dictionary_add(dictionary_array,"two");
     dictionary_add(dictionary_array,"three");
-    TTEST_ASSERT_EQUAL_INT(0,dictionary_is_present(dictionary_array,"for"));
+    printf("%d",dictionary_is_present(dictionary_array,"for"));
+    //TEST_ASSERT_EQUAL_INT(0,dictionary_is_present(dictionary_array,"for"));
+    dictionary_array_free(dictionary_array);
+}*/
+
+static void test_search(){
+    Dictionary * dictionary_array = dictionary_create();
+    dictionary_add(dictionary_array,"one");
+    dictionary_add(dictionary_array,"two");
+    TEST_ASSERT_EQUAL_INT(0,dictionary_search(dictionary_array,0,dictionary_array_size(dictionary_array),"one")); 
     dictionary_array_free(dictionary_array);
 }
 
+static void test_search_two(){
+    Dictionary * dictionary_array = dictionary_create();
+    dictionary_add(dictionary_array,"one");
+    dictionary_add(dictionary_array,"two");
+    dictionary_add(dictionary_array,"three");
+    dictionary_add(dictionary_array,"for");
+    TEST_ASSERT_EQUAL_INT(2,dictionary_search(dictionary_array,0,dictionary_array_size(dictionary_array),"three")); 
+    dictionary_array_free(dictionary_array);
+}
+
+static void test_get_element(){
+    Dictionary * dictionary_array = dictionary_create();
+    dictionary_add(dictionary_array,"one");
+    dictionary_add(dictionary_array,"two");
+    dictionary_add(dictionary_array,"three");
+    dictionary_add(dictionary_array,"for");
+    TEST_ASSERT_EQUAL_STRING("for",dictionary_get_elem(dictionary_array,3)); 
+    dictionary_array_free(dictionary_array);
+
+}
 
 int main(){
     UNITY_BEGIN();
@@ -49,7 +79,10 @@ int main(){
     RUN_TEST(test_dictionary_array_size_one_el);
     RUN_TEST(test_dictionary_array_size_two_el);
     RUN_TEST(test_present);
-    RUN_TEST(test_not_present);
-
+    //RUN_TEST(test_not_present);
+    RUN_TEST(test_search);
+    RUN_TEST(test_search_two);
+    RUN_TEST(test_get_element);
+    
     return UNITY_END();
 }
